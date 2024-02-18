44c69f2a35900a5d36ee465a03009bb949bfdb4b
Anand Suthar <anandsuthar956@gmail.com>
2024-02-18 20:38:04 +0530

    Add TodoApp

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import TodoApp from "./components/TodoApp";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <>Todo</>;
+  return (
+    <>
+      {" "}
+      <TodoApp />
+    </>
+  );
 }
 
 export default App;
