9e1440625ee8ae65cfb9be4e42089f16d64e60c9
Anand Suthar <anandsuthar956@gmail.com>
2024-02-23 21:57:49 +0530

    initial

---
+++ auth/src/components/LoginForm.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../features/userSlice";
+
+function LoginForm() {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch(login());
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Email..."
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password..."
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
