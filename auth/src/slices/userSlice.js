d7557deb141ca97060540bcf6029af2e62d78b84
Anand Suthar <anandsuthar956@gmail.com>
2024-02-23 21:49:09 +0530

    added user slice

---
+++ auth/src/slices/userSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isAuthenticated: false,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+// Exporting actions
+export const { login, logout } = userSlice.actions;
+
+// Exporting reducer
+export default userSlice.reducer;
