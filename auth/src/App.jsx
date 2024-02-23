bf8ae699ce36a4f0e7348db00b0ffd9eeef6085d
Anand Suthar <anandsuthar956@gmail.com>
2024-02-23 22:05:00 +0530

    Added Nabar and Loginform

--- auth/src/App.jsx
+++ auth/src/App.jsx
@@ -1,6 +1,14 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
+import LoginForm from "./components/LoginForm";
 function App() {
-  return <>Authentication App</>;
+  return (
+    <>
+      <h1>Authentication App</h1>
+      <NavBar />
+      <LoginForm />
+    </>
+  );
 }
 
 export default App;
