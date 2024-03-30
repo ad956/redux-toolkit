8bcc12b706997f4994b9781a5dca2400749010bb
Anand Suthar <anandsuthar956@gmail.com>
2024-03-30 14:31:14 +0530

    Added combineReducers to use both reducers

--- todo-app/src/app/store.js
+++ todo-app/src/app/store.js
@@ -1,6 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import todoReducer from "../features/todo/todoSlice";
+import todoFetchReducer from "../features/todoFetch/todoFetchSlice";
+
+const rootReducer = combineReducers({
+  todo: todoReducer,
+  todosfetch: todoFetchReducer,
+});
 
 export const store = configureStore({
-  reducer: todoReducer,
+  reducer: rootReducer,
 });
