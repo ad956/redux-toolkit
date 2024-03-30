006c35b676dc43fb987b6fb3d6ed3d961c69fcb0
Anand Suthar <anandsuthar956@gmail.com>
2024-03-30 14:30:08 +0530

    Added both todos component

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,12 +1,18 @@
 import TodoApp from "./components/TodoApp";
+import TodoFetchApp from "./components/TodoFetchApp";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      {" "}
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-evenly",
+      }}
+    >
       <TodoApp />
-    </>
+      <TodoFetchApp />
+    </div>
   );
 }
 
